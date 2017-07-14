68938e5066fdf671676d383c0fceaabb5186b468
Ray Cui <ray.cui@themotorreport.com.au>
2017-07-14 16:38:37 +1000

    add tests

---
+++ src/Ray/Payroll/Math.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Simple example math class
+ *
+ * @package PhpUnit
+ * @license MIT License <http://opensource.org/licenses/mit-license.html>
+ */
+namespace Ray\Payroll;
+
+/**
+ * Math
+ *
+ * @author Michael Funk <mike@mikefunk.com>
+ */
+class Math
+{
+
+    /**
+     * add two numbers together. Wow. Such complicated.
+     *
+     * @param int $number1 the first number to add
+     * @param int $number2 the second number to add
+     * @return int
+     */
+    public function add($number1, $number2)
+    {
+        return $number1 + $number2;
+    }
+}
---
+++ tests/EmployeeTest.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Employee PHPUnit test
+ *
+ * @package AutoClassifiedsPlatform
+ * @copyright 2015 Internet Brands, Inc. All Rights Reserved.
+ */
+namespace Tests;
+
+use Ray\Payroll\Employee;
+use Ray\Payroll\PayslipProcessor;
+use Ray\Payroll\PayslipInfo;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * EmployeeTest
+ *
+ * @author Ray Cui <cuidream@hotmail.com>
+ */
+class EmployeeTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * first name
+     *
+     * @var $first_name
+     */
+    private $firstName = 'John';
+	
+	/**
+     * last name
+     *
+     * @var $first_name
+     */
+    private $lastName = 'Smith';
+	private $annualSalary = 60050;
+	private $superRate = 9;
+	private $paymentStartDate = '11-02-2016';
+	private $oEmployee;
+	private $oProcessor;
+	
+	/**
+     * create an employee instance
+     *
+     * @test
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->oEmployee = new Employee($firstName, $lastName, $annualSalary, $superRate, $paymentStartDate);
+    }
+
+    /**
+     * itAddsTwoNumbers test
+     *
+     * @test
+     * @return void
+     */
+    public function testEmployeeAttributes()
+    {
+		$this->assertEquals($firstName, $this->oEmployee->getFirstName());
+		$this->assertEquals($lastName, $this->oEmployee->getLastName());
+		$this->assertEquals($annualSalary, $this->oEmployee->getAnnualSalary());
+		$this->assertEquals($annualSalary, $this->oEmployee->getSuperRate());
+		$this->assertEquals($paymentStartDate, $this->oEmployee->getPaymentStartDate());
+    }
+	
+	public function testConstructorCalls() {
+		$classname = 'Employee';
+		$payslipInfo = [];
+		$oEmployee_1 = new Employee($this->firstName, $this->lastName, $this->annualSalary, $this->superRate, $this->paymentStartDate);
+		$oEmployee_2 = new Employee('first name', 'last name', 37000, 9, '21-07-2014');
+		//var_dump($oEmployee);
+		
+		$test = array($oEmployee_1, $oEmployee_2);
+		
+		var_dump($test);
+		/*$mockProcessor = $this->getMockBuilder('\Ray\Payroll\PayslipProcessor')
+		  ->setMethods(array('__construct'))
+		  ->setConstructorArgs((array)$test)	
+		  ->getMock();*/
+		$obj = new PayslipProcessor($test);
+		var_dump($obj->getEmployees());
+		//$result[0] = $mockProcessor->generatePayslip();
+		//var_dump($result[0][0]);
+		//$object = $result[0];
+		//$this->assertEquals();
+		
+	}
+	
+}
+
